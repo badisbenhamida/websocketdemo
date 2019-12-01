fac7b002de1e3d763272302f6334ebb4b7a3a879
badis <badis@visualscm.com>
2019-12-01 18:54:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import Sockette from 'sockette';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'websocketdemo';
+export class AppComponent implements OnInit { 
+  title = "Web Socket Demo";	
+  private s: Sockette;
+constructor(private router: Router) {}
+  ngOnInit() {
+  this.s = new Sockette('wss://jrmugob5va.execute-api.us-east-1.amazonaws.com/prod', {
+  timeout: 5e3,
+  maxAttempts: 10,
+  onopen: e => console.log('Connected!', e),
+  onmessage: e => {
+    console.log('Received:', e);
+    this.router.navigate(['/' + e.data]).then(nav => {
+    console.log(nav); // true if navigation is successful
+  }, err => {
+    console.log(err) // when there's an error
+  });
+  },
+  onreconnect: e => console.log('Reconnecting...', e),
+  onmaximum: e => console.log('Stop Attempting!', e),
+  onclose: e => console.log('Closed!', e),
+  onerror: e => console.log('Error:', e)
+});
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
